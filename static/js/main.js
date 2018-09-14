cc2c1794f1154b5feca69464a20796a1db7ee6cf
Aik Boba <>
2018-09-15 00:32:32 +0300

    Improvements

--- static/js/main.js
+++ static/js/main.js
@@ -9,11 +9,27 @@
         var width = document.getElementById(elId).clientWidth;
         var contentHeight = document.getElementById(elId).scrollHeight;
         var contentWidth = document.getElementById(elId).scrollWidth;
-        if ( contentHeight > height || contentWidth > width )
+        if ( (contentHeight > height || contentWidth > width) && labelsList[i].childElementCount > 1 )
         {
             document.querySelector('[data-for = "' + elId + '"]').classList.add("active");
         }
     }
+    //Items scrolling
+    /*var labelsScrollers = document.getElementsByClassName('label-list-scroller');
+    for (var i = 0; i < labelsScrollers.length; i++)
+    {
+        labelsScrollers[i].addEventListener('click', function() {
+            var target = this.getAttribute("data-for");
+            var method = this.getAttribute("data-method");
+            var direct = this.getAttribute("data-direct");
+            var itemsList = document.getElementById(target);
+            var itemsCount = itemsList.childElementCount;
+            var items = itemsList.getElementsByClassName('label-item');
+            var currentItem = items[1];
+
+            scrollToElm( items, currentItem , 600 );
+        });
+    }*/
 
     //Popups
     var popupsList = document.getElementsByClassName('label-item');
@@ -152,6 +168,7 @@
     }
 
     //Functions
+    //Popups
     function fadeIn(el, time) {
         el.style.opacity = 0;
         el.style.display = 'flex';
@@ -184,5 +201,44 @@
         tick();
         el.style.display = 'none';
     }
+    //Scroll
+    function scrollToElm(container, elm, duration){
+        var pos = getRelativePos(elm);
+        scrollTo( container, pos.top , 2);  // duration in seconds
+    }
+    function getRelativePos(elm){
+        var pPos = elm.parentNode.getBoundingClientRect(), // parent pos
+            cPos = elm.getBoundingClientRect(), // target pos
+            pos = {};
+
+        pos.top    = cPos.top    - pPos.top + elm.parentNode.scrollTop,
+            pos.right  = cPos.right  - pPos.right,
+            pos.bottom = cPos.bottom - pPos.bottom,
+            pos.left   = cPos.left   - pPos.left;
+
+        return pos;
+    }
+    function scrollTo(element, to, duration, onDone) {
+        var start = element.scrollTop,
+            change = to - start,
+            startTime = performance.now(),
+            val, now, elapsed, t;
+
+        function animateScroll(){
+            now = performance.now();
+            elapsed = (now - startTime)/1000;
+            t = (elapsed/duration);
+
+            element.scrollTop = start + change * easeInOutQuad(t);
+
+            if( t < 1 )
+                window.requestAnimationFrame(animateScroll);
+            else
+                onDone && onDone();
+        };
+
+        animateScroll();
+    }
+    function easeInOutQuad(t){ return t<.5 ? 2*t*t : -1+(4-2*t)*t }
 
 })();
